f991d1ac9fed401a79dd01439b99a3324be97cac
Faris Ansara <ansara.fares@gmail.com>
2024-09-06 18:41:43 +0300

    refactor: cleaning up code

--- algorithms/MultiAgent.py
+++ algorithms/MultiAgent.py
@@ -1,18 +1,18 @@
 from algorithms.classical.floodfill.FloodFillTime import FloodFillTime
 from algorithms.classical.bfs.BFSTime import BFSTime
-from algorithms.reinforcement_learning.dynaq.DynaQSarsa import DynaQSarsa
+from algorithms.reinforcement_learning.dynaq.DynaQLearning import DynaQLearning
 from algorithms.utilities.Stats import Stats
 
 
-class MultiAgent():
+class MultiAgent:
 
     def __init__(self, walls, maze_height=16, maze_width=16):
         flood = FloodFillTime(walls=walls, maze_height=maze_height, maze_width=maze_width)
         bfs = BFSTime(walls=walls, maze_width=maze_width, maze_height=maze_height)
-        dyna = DynaQSarsa(walls=walls)
+        dyna = DynaQLearning(walls=walls)
         flood_path = flood.get_path_from_flood_map()
         bfs_path = bfs.find_shortest_path_to_goal()
-        dyna.run_dyna_sarsa()
+        dyna.run_dyna_qlearning()
         dyna_path = dyna.get_path()
         self.paths = [flood_path, bfs_path, dyna_path]
         self.path = None
